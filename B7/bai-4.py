8aceec6f66e8f38d03e116377b36bed435a879bf
whis102 <chicongnguyen.dev@gmail.com>
2024-12-07 19:40:38 +0700

    update

--- B7/bai-4.py
+++ B7/bai-4.py
@@ -1,5 +1,6 @@
 """
-Ta đã biết cách cài đặt của thuật toán Merge Sort qua phương pháp đệ quy. Phương pháp này được sử dụng phổ biến do dễ cài đặt, tuy nhiên ta lại tốn O(log(n)) độ phức tạp bộ nhớ stack cho việc gọi đệ quy.
+Ta đã biết cách cài đặt của thuật toán Merge Sort qua phương pháp đệ quy. 
+Phương pháp này được sử dụng phổ biến do dễ cài đặt, tuy nhiên ta lại tốn O(log(n)) độ phức tạp bộ nhớ stack cho việc gọi đệ quy.
 
 Ta cũng biết rằng mọi thuật toán đệ quy đều có thể được viết dưới dạng không đệ quy.
 
@@ -22,3 +23,21 @@ def show_list(arr, title=''):
 
 sample_number_list = [5, 9, 1, 12, 30, 35, 75, 10, 15, 20, 4, 0, 20, 0, 20, 3, 6, 14]
 show_list(sample_number_list, title='sample_number_list')
+
+def merge(arr, left, right, mid ):
+    # Tạo hai mảng con cho phần cần hợp nhất
+    arr1 = arr[left:mid]
+    arr2 = arr[mid:right]
+
+    # Tạo ra biến để đo độ dài
+
+
+    # while i < n1 and j < n2:
+
+def merge_sort(arr):
+    # Kích thước mảng con ban đầu là 1
+
+# print mảng lúc đầu
+# print mảng lúc sau
+
+# show_list(number_list_2, title='number_list_2')
